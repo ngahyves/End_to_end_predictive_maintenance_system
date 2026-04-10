f4fa3130cc192be0c44f86bd5e2992afcc3afbaa
NGAH Yves-Bernard-Simplice <rybsngah@yahoo.fr>
2026-04-10 07:03:14 +0200

    fix: preprocessing tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,21 +1,30 @@
-# RAM optimization and  required columns
-from src.utils.preprocessing.preprocess import Preprocessor
+import pytest
+import pandas as pd
 import numpy as np
+from src.utils.preprocessing.preprocess import Preprocessor
+from src.utils.config_loader import load_config
 
-def test_optimize_dtypes(sample_data):
-    """Vérify the conversion from float64-> float32."""
-    from src.utils.config_loader import load_config
+def test_preprocessing_output_shape(sample_data, tmp_path):
+    """Verify data shape without the real csv."""
+    # 1. load config
     cfg = load_config()
-    proc = Preprocessor(cfg)
+
+    # 2. Create a Mocking the disk)
+    temp_data_dir = tmp_path / "data" / "raw"
+    temp_data_dir.mkdir(parents=True)
+    temp_csv_path = temp_data_dir / "ai4i2020.csv"
     
-    optimized_df = proc._optimize_dtypes(sample_data)
-    assert optimized_df["Air temperature [K]"].dtype == np.float32
+    sample_data.to_csv(temp_csv_path, index=False)
 
-def test_preprocessing_output_shape(sample_data):
-    """Vérifying if we have the right number of columns."""
-    from src.utils.config_loader import load_config
-    cfg = load_config()
+    # 3. Analysing the mocking file
+    cfg["paths"]["raw_data_path"] = str(temp_csv_path)
+    cfg["paths"]["processed_data_dir"] = str(tmp_path / "processed")
+    cfg["paths"]["processor_path"] = str(tmp_path / "preprocessor.joblib")
+
+    # 4. Processor execution
     proc = Preprocessor(cfg)
-    # Run simulation
-    X_train, _, _, _ = proc.run() 
+    X_train, X_test, y_train, y_test = proc.run()
+
+    # 5. Assertion
     assert isinstance(X_train, np.ndarray)
+    assert X_train.shape[1] > 0  # Verification
